ab8e82a127f05dd8b0744ea634a75fc5c6401d31
chwech <804699297@qq.com>
2018-11-19 14:34:01 +0800

    添加辅助函数转换url

--- web/src/utils/utils.js
+++ web/src/utils/utils.js
@@ -2,3 +2,19 @@
  * 空函数
  */
 export function noop () {}
+
+/**
+ * 拼接url, 在url后面拼参数，例如：/test  =>  /test/1
+ * @export
+ * @param {String} url
+ * @param {Array} params
+ * @returns url
+ */
+export function transferUrl (url, params) {
+  if (Array.isArray(params)) {
+    for (param of params) {
+      url += `/${param}`
+    }
+  }
+  return url
+}
